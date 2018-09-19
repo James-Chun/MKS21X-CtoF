d1a45b510a22f059971ae75e41b1a6c56f6b4fe3
James-Chun <jchun00@stuy.edu>
2018-09-18 22:03:05 -0400

    9/18 HW

--- CtoFTester.java
+++ CtoFTester.java
@@ -4,10 +4,12 @@ public class CtoFTester {
     }
     
     public static int celsiusToFahrenheit(int C){
-        return (C * 5.0) / 9.0 + 32.0;
+        return (C * 5) / 9 + 32;
     }
     
     public static void main(String[] args){
-    System.out.println(celsiusToFahrenheit(1));}
+    System.out.println(celsiusToFahrenheit(1));
+    System.out.println(fahrenheitToCelsius(100));
+    }
 }
 
